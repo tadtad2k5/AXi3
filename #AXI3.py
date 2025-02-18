3b0b9a2e711d89ce9247db99eb9a472a87596195
tadtad2k5 <23520244@gm.uit.edu.vn>
2025-02-18 21:10:16 +0700

    class AXI3 + class matser

--- #AXI3.py
+++ #AXI3.py
@@ -3,3 +3,38 @@
 # Dat - Uy
 # 17/02/2025
 print("Project AXI3")
+#AXI3 : Master side and Slave side
+class AXI3:
+    def __init_(self):
+        self.AR = ""
+        self.R = ""
+        self.AW = ""
+        self.W = ""
+        self.B = ""
+        # = VAILD/Ready
+'''   Kênh AR sẽ có tín hiệu ARVALID (master) và tín hiệu ARREADY (slave).
+Kênh R sẽ có tín hiệu RVALID (slave) và tín hiệu RREADY (master) .
+Kênh AW sẽ có tín hiệu AWVALID (master) và tín hiệu AWREADY (slave). 
+Kênh W sẽ có tín hiệu WVALID (master) và tín hiệu WREADY (slave) .
+Kênh B sẽ có tín hiệu BVALID (slave) và tín hiệu BREADY (master) .
+'''
+#
+class Master(AXI3):
+    def __init__(self):
+        super().__init__()
+        self.AR = "ARVAILD"
+        self.R = "RREADY"
+        self.AW = "AWVAILD"
+        self.W = "WVAILD"
+        self.B = "BREADY"
+    def read_address_channel(self):
+        if self.AR == "ARVAILD":
+            return True
+        return False
+    
+    
+
+
+    
+    
+        

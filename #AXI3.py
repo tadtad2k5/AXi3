e573c315a3c5609b61845315c23eb8c71804d213
tadtad2k5 <147678976+tadtad2k5@users.noreply.github.com>
2025-02-19 15:26:12 +0700

    Update #AXI3.py
    hoàn thiện thêm các biến, thêm giá trị

--- #AXI3.py
+++ #AXI3.py
@@ -11,6 +11,16 @@ class AXI3:
         self.AW = ""
         self.W = ""
         self.B = ""
+        self.ARVALID = False
+        self.ARREADY = False
+        self.RVALID = False
+        self.RREADY = False
+        self.AWVALID = False
+        self.AWREADY = False
+        self.WVALID = False
+        self.WREADY = False
+        self.BVALID = False
+        self.BREADY = False
         # = VAILD/Ready
 '''   Kênh AR sẽ có tín hiệu ARVALID (master) và tín hiệu ARREADY (slave).
 Kênh R sẽ có tín hiệu RVALID (slave) và tín hiệu RREADY (master) .
@@ -22,13 +32,34 @@ Kênh B sẽ có tín hiệu BVALID (slave) và tín hiệu BREADY (master) .
 class Master(AXI3):
     def __init__(self):
         super().__init__()
-        self.AR = "ARVAILD"
+        self.AR = "ARVALID"
         self.R = "RREADY"
-        self.AW = "AWVAILD"
-        self.W = "WVAILD"
+        self.AW = "AWVALID"
+        self.W = "WVALID"
         self.B = "BREADY"
+
     def read_address_channel(self):
-        if self.AR == "ARVAILD":
+        if self.AR == "ARVALID":
+            return True
+        return False
+
+    def write_address_channel(self):
+        if self.AW == "AWVALID":
+            return True
+        return False
+
+    def write_data_channel(self):
+        if self.W == "WVALID":
+            return True
+        return False
+
+    def read_data_channel(self):
+        if self.R == "RREADY":
+            return True
+        return False
+
+    def write_response_channel(self):
+        if self.B == "BREADY":
             return True
         return False
     
